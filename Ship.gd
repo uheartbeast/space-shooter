d5372e6c832f4f212967a73baf690c669c101dec
Benjamin Anderson <heartbeast.studios@gmail.com>
2019-09-05 15:50:38 -0600

    Ridged Bodies + Instancing

--- Ship.gd
+++ Ship.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+const Bullet = preload("res://Bullet.tscn")
+
 export(int) var SPEED = 100
 
 func _process(delta):
@@ -7,3 +9,11 @@ func _process(delta):
 		position.y -= SPEED * delta
 	if Input.is_action_pressed("ui_down"):
 		position.y += SPEED * delta
+	if Input.is_action_just_pressed("ui_accept"):
+		fire_bullet()
+
+func fire_bullet():
+	var bullet = Bullet.instance()
+	var main = get_tree().current_scene
+	main.add_child(bullet)
+	bullet.global_position = global_position
